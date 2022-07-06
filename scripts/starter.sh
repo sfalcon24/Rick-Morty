af3d05d72201c0bdfa3018679c2ec4df61fd2915
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-07-06 16:30:12 +0200

    chore: add initial script

---
+++ scripts/starter.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+INIT_NAME="rnapp"
+PROJECT_NAME=""
+PROJECT_NAME_LC=""
+GIT_USER=$(git config user.name)
+GIT_EMAIL=$(git config user.email)
+
+if [ "$GIT_USER" == "" ]
+then 
+    echo "No Git user found."
+    exit;
+fi
+
+if [ "$GIT_EMAIL" == "" ]
+then 
+    echo "No Git mail found."
+    exit;
+fi
+
+echo ""
+echo "A new journey begins, what's the name?:"
+
+read -p "" PROJECT_NAME
+
+PROJECT_NAME_LC=$(echo "$PROJECT_NAME" | tr '[:upper:]' '[:lower:]')
+
+if [ -z "$PROJECT_NAME" ]
+then 
+    echo "No project's name found. Exiting..."
+    exit;
+fi
+
+yarn
+cd ios && pod install && cd ..
+
+sed -i '' "s/React Native Template/$PROJECT_NAME_LC/g" README.md
+
+echo "Changing app.json name to $PROJECT_NAME_LC..."
+sed -i '' "s/${INIT_NAME}/$PROJECT_NAME_LC/g" app.json
+
+echo "Changing package.json name to $PROJECT_NAME_LC..."
+sed -i '' "s/${INIT_NAME}/$PROJECT_NAME_LC/g" package.json
+
+echo "Changing Android name to $PROJECT_NAME_LC..."
+sed -i '' "s/${INIT_NAME}/$PROJECT_NAME_LC/g" android/app/src/main/res/values/strings.xml
+
+echo "Changing iOS name to $PROJECT_NAME_LC..."
+sed -i '' "s/${INIT_NAME}/$PROJECT_NAME_LC/g" "ios/${INIT_NAME}/Info.plist"
+
+echo "Changing iOS launchscreen to $PROJECT_NAME_LC..."
+sed -i '' "s/${INIT_NAME}/$PROJECT_NAME_LC/g" "ios/${INIT_NAME}/LaunchScreen.storyboard"
+
+echo "Removing init script"
+sed '6d' package.json | tee package.json
+
+git checkout --orphan latest_branch
+git add -A
+git rm -f starter.sh
+git commit -am "chore: Initial commit"
+git branch -D master
+git branch -m master
+
+echo ""
+echo "Don't forget to upload to the repository!"
+echo ""
+echo "## git push -u origin master --force"
+echo ""
