1683a6962ef86d9d408874902c52d3f25435ed11
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-07-07 13:45:08 +0200

    chore: add hook form

---
+++ src/utils/useCustomForm/index.ts
@@ -0,0 +1,85 @@
+import {useMemo} from 'react';
+import {yupResolver} from '@hookform/resolvers/yup';
+import type {
+  DeepPartial,
+  Path,
+  SubmitErrorHandler,
+  SubmitHandler,
+} from 'react-hook-form';
+import {useForm} from 'react-hook-form';
+import type {ObjectSchema} from 'yup';
+import type {CustomError} from '../error';
+
+type LogicParams<T> = {
+  defaultValues: DeepPartial<T>;
+  onSubmit?: SubmitHandler<T>;
+  onError?: SubmitErrorHandler<T>;
+  validationSchema?: ObjectSchema<any>;
+  resetOnSubmit?: boolean;
+  promptInputErrors?: boolean;
+  handleServerError?: boolean;
+};
+
+export const useCustomForm = <T>({
+  defaultValues,
+  onSubmit,
+  onError,
+  validationSchema,
+  resetOnSubmit = false,
+  promptInputErrors = true,
+  handleServerError = false,
+}: LogicParams<T>) => {
+  const formMethods = useForm<T>({
+    defaultValues,
+    resolver: validationSchema ? yupResolver(validationSchema) : undefined,
+    mode: 'onSubmit',
+  });
+  const {handleSubmit, reset, setError} = formMethods;
+
+  const submit = useMemo(
+    () =>
+      handleSubmit(async (onValid, onInvalid) => {
+        try {
+          if (onSubmit) {
+            await onSubmit(onValid, onInvalid);
+            if (resetOnSubmit) {
+              reset();
+            }
+          }
+        } catch (err) {
+          const error = err as CustomError;
+          if (promptInputErrors) {
+            Object.entries(error.fields ?? {}).forEach(([field, errors]) => {
+              setError(field as Path<T>, {
+                message: errors.join('\n'),
+              });
+            });
+          }
+          if (handleServerError) {
+            setError('serverError' as Path<T>, {
+              message: error.message,
+            });
+            return;
+          }
+          throw err as CustomError;
+        }
+      }, onError),
+    [
+      handleSubmit,
+      onError,
+      onSubmit,
+      resetOnSubmit,
+      reset,
+      promptInputErrors,
+      handleServerError,
+      setError,
+    ],
+  );
+
+  return {
+    ...formMethods,
+    formMethods,
+    submit,
+    isValid: formMethods.formState.isValid,
+  };
+};
