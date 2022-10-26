6a351d2722f78c520d6703a6bffc56dbfbe31fab
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-10-26 13:43:48 +0200

    chore: update env template

--- types/react-native-config.d.ts
+++ types/react-native-config.d.ts
@@ -1,6 +1,6 @@
 declare module 'react-native-config' {
   interface Env {
-    ENVIRONMENT: string;
+    ENVIRONMENT: 'DEV' | 'STG' | 'PROD';
     ANDROID_KEY_PASSWORD: string;
     ANDROID_STORE_PASSWORD: string;
     GRAPHQL_ENDPOINT: string;
