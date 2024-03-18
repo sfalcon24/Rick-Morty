37918fa06150dc17262415389024254f894531e6
sfalcon24 <sergiofalcon@z1.digital>
2024-03-18 12:50:37 +0100

    feat: button issues solved 7

--- src/common/ui/components/TextButton/index.tsx
+++ src/common/ui/components/TextButton/index.tsx
@@ -1,14 +1,13 @@
 import type {FC} from 'react';
 import {memo} from 'react';
 import React from 'react';
-import Icons from '../Icons';
-import {Container, LeftAction, Pressable, Title} from './styles';
+import {Container, Pressable, Title, Icon} from './styles';
 import type {Props} from './types';
 
 const Button: FC<Props> = ({loading, onPress, style, title, leftIcon}) => (
   <Container style={style}>
     <Pressable enabled={!loading && !!onPress} onPress={onPress}>
-      <LeftAction>{leftIcon && <Icons name={leftIcon} />}</LeftAction>
+      {leftIcon && <Icon name={leftIcon} />}
       <Title>{title}</Title>
     </Pressable>
   </Container>
