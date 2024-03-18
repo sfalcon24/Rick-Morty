3c44f70ea0dd2c32b823d082fecf5ada472c36e6
sfalcon24 <sergiofalcon@z1.digital>
2024-03-18 13:14:04 +0100

    feat: cards issues solved 4

--- src/common/ui/components/Cards/CharacterCard/types.ts
+++ src/common/ui/components/Cards/CharacterCard/types.ts
@@ -1,10 +1,7 @@
-import type {StyleProp, ViewStyle} from 'react-native';
-
 export type Props = {
   id: string;
   status: 'Alive' | 'Dead' | 'unknown';
   name: string;
   image?: string;
   onPress?: (id: string) => void;
-  style?: StyleProp<ViewStyle>;
 };
